5561308db996104fd35120d791fbec13d0f14730
porrinha09 <142546604+porrinha09@users.noreply.github.com>
2024-02-24 17:56:48 -0300

    Update Clear Caps Lock.js

--- Plugins/Clear Caps Lock.js
+++ Plugins/Clear Caps Lock.js
@@ -1,1 +1,30 @@
-a
+// ==UserScript==
+// @name Clear caps lock
+// @description Better YouTube
+// @author Kelvin zv
+// @version 1.0.0
+// @grant GM_addStyle
+// @run-at document-start
+// @include http://youtube.com/*
+// @include https://youtube.com/*
+// @include http://*.youtube.com/*
+// @include https://*.youtube.com/*
+// @downloadURL https://raw.githubusercontent.com/porrinha09/Better-YouTube/main/Plugins/Clear%20Caps%20Lock.js
+// @updateURL https://raw.githubusercontent.com/porrinha09/Better-YouTube/main/Plugins/Clear%20Caps%20Lock.js
+// ==/UserScript==
+
+(function() {
+let css = `
+  .title, #video-title
+  {
+    text-transform: lowercase;
+  }
+`;
+if (typeof GM_addStyle !== "undefined") {
+  GM_addStyle(css);
+} else {
+  let styleNode = document.createElement("style");
+  styleNode.appendChild(document.createTextNode(css));
+  (document.querySelector("head") || document.documentElement).appendChild(styleNode);
+}
+})();
