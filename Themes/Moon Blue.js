495d1a731274fb00e4da4eeb7fad53f7e2a0119c
porrinha09 <142546604+porrinha09@users.noreply.github.com>
2024-02-23 23:32:48 -0300

    Update Moon Blue.js

--- Themes/Moon Blue.js
+++ Themes/Moon Blue.js
@@ -1,13 +1,12 @@
-
 // ==UserScript==
 // @name         Moon Blue
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  Better YouTube
 // @icon         https://raw.githubusercontent.com/porrinha09/Better-YouTube/main/assets/BetterYouTube.png
-// @author       Kelvin
-// @downloadURL https://raw.githubusercontent.com/porrinha09/Better-YouTube/main/Plugins/View%20deslikes.js
-// @updateURL    https://raw.githubusercontent.com/porrinha09/Better-YouTube/main/Plugins/View%20deslikes.js
+// @author       Kelvin zv
+// @downloadURL https://raw.githubusercontent.com/porrinha09/Better-YouTube/main/Themes/Moon%20Blue.js
+// @updateURL    https://raw.githubusercontent.com/porrinha09/Better-YouTube/main/Themes/Moon%20Blue.js
 // @match        https://www.youtube.com/*
 // @grant        GM_addStyle
 // ==/UserScript==
