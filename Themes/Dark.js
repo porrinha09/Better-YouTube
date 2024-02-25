a4915810e749b62d2590f03a140397e51261ef8c
porrinha09 <142546604+porrinha09@users.noreply.github.com>
2024-02-25 14:57:59 -0300

    Update Dark.js

--- Themes/Dark.js
+++ Themes/Dark.js
@@ -1,1 +1,26 @@
-a
+// ==UserScript==
+// @name         Dark
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Better YouTube
+// @icon         https://raw.githubusercontent.com/porrinha09/Better-YouTube/main/assets/BetterYouTube.png
+// @author       Kelvin zv
+// @match        https://www.youtube.com/*
+// @downloadURL https://raw.githubusercontent.com/porrinha09/Better-YouTube/main/Themes/Dark.js
+// @updateURL    https://raw.githubusercontent.com/porrinha09/Better-YouTube/main/Themes/Dark.js
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    GM_addStyle(`
+        body, #page-manager {
+            background-color: #000000 !important;
+            color: #fff !important;
+        }
+        ytd-masthead, #masthead-container {
+            background-color: #000000 !important;
+        }
+    `);
+})();
