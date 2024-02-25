3e56eb10f2ac2c8de152da812862e3a7573c89bb
porrinha09 <142546604+porrinha09@users.noreply.github.com>
2024-02-25 16:26:22 -0300

    Update Hide comments.js

--- Plugins/Hide comments.js
+++ Plugins/Hide comments.js
@@ -1,108 +1,40 @@
 // ==UserScript==
-// @name        Hide comments
-// @description Better YouTube
-// @include     /^http(|s)://www\.youtube\.com/watch\?v=.*$/
-// @grant       none
-// @author      Dio
-// @icon         https://raw.githubusercontent.com/porrinha09/Better-YouTube/main/assets/BetterYouTube.png
-// @version     1.0.0
-// @grant       none
-// @namespace   38a3e9648fc5260c75446e376a8dabb3
+// @name         Hide comments
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Better YouTube
+// @author       Kelvin zv
+// @include     http://www.youtube.com/*
+// @include     https://www.youtube.com/*
+// @grant        none
 // @downloadURL https://raw.githubusercontent.com/porrinha09/Better-YouTube/main/Plugins/Hide%20comments.js
 // @updateURL https://raw.githubusercontent.com/porrinha09/Better-YouTube/main/Plugins/Hide%20comments.js
 // ==/UserScript==
 
-function XBLoad(func, verbose)
-{
-    verbose = verbose || false;
+let removedComments = false;
+let timerComments;
+const comments_id = 'ytd-comments.style-scope';
 
-    if (window.jQuery)
-    {
-        if ($(window).load)
-        {
-            if (verbose == true) { console.log('Javascript loaded using $(window).load method'); };
-            return $(window).load(function() { func(); });
-        }
-        else if ($(window).ready)
-        {
-            if (verbose == true) { console.log('Javascript loaded using $(window).ready method'); };
-            return $(window).ready(function() { func(); });
-        };        
-    }
-    else if (document.addEventListener)
-    {
-        if (verbose == true) { console.log('Javascript loaded using document.addEventListener method'); };
-        document.addEventListener('DOMContentLoaded', function(event)
-        {
-            return func();
-        });
-    }
-    else if (document.attachEvent)
-    {
-        if (verbose == true) { console.log('Javascript loaded using document.attachEvent method'); };
-        document.attachEvent('load', function()
-        {
-            return func();
-        });
-    }
-    else
-    {
-        if (verbose == true) { console.log('Javascript loaded using setTimeout method'); };
-        return setTimeout(function() { func(); }, 5000);
-    };
-};
+(function() {
+    'use strict';
+    console.log('youtube-comment-remover started');
+})();
 
-function searchStringInArray (str, arr)
-{
-    var obj = {};
-    for (var i=0; i<arr.length; i++)
-    {
-        if (arr[i].match(str))
-        {
-            obj.idx = i;
-            obj.content = arr[i];
-            return obj;
-        }
-    }
-    return -1;
+removeComments = function() {
+    "use strict";
+    let comments = document.querySelector(comments_id);
+    comments.parentNode.removeChild( comments );
+    document.body.removeEventListener('DOMSubtreeModified', timerComments, false);
 }
 
-function delElement (elmt_type, elmt_name)
-{
-    switch (elmt_type)
-    {
-        case 'id':
-            var tgt = document.getElementById(elmt_name);
-            break;
-        case 'tag':
-            var tgt = document.getElementsByTagName(elmt_name)[0];
-            break;
-        case 'class':
-            var tgt = document.getElementsByClassName(elmt_name)[0];
-            break;
-        default:
-            console.log("delElement syntax error\nUsage:\n  delElement(<[id|tag|class]>, <elmt_name>)");
-            return -1;
-    }
-    tgt.parentNode.removeChild(tgt);
-}
-
-function delIdxFromArrayByValue (str, arr)
-{
-    if (searchStringInArray(str, arr))
-    {
-        arr.splice(searchStringInArray(str, arr).idx, 1);
+timerComments = function () {
+    if(!removedComments) {
+      let comments = document.querySelector(comments_id);
+      if(comments !== null) {
+          setTimeout(removeComments, 2400);
+          removedComments = true;
+      }
     }
 }
 
-function showtime ()
-{
-    delElement('id', 'watch-discussion');
-    
-    var dscNode = document.getElementById('action-panel-details');
-    var dscClassesArr = dscNode.className.split(' ');
-    delIdxFromArrayByValue('yt-uix-expander-collapsed', dscClassesArr);
-    dscNode.className = dscClassesArr.join(' ');
-}
-
-XBLoad(showtime());
+document.body.addEventListener('DOMSubtreeModified', timerComments, false);
