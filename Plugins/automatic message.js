b5deee55aa3acdd8530ce42bb638acc3a2aa1f5a
porrinha09 <142546604+porrinha09@users.noreply.github.com>
2024-02-26 03:43:22 -0300

    Update automatic message.js

--- Plugins/automatic message.js
+++ Plugins/automatic message.js
@@ -1,1 +1,15 @@
-a
+// ==UserScript==
+// @name         Troll yourself if your name is jim
+// @version       1.2.6
+// @description  If your name is jim and you want to troll yourself whenever you type www.google.com or www.youtube.com in the serch bar it will display some text to try to troll yourself when the page loads
+// @author       Theawsomeyellowcatinthehat
+// @match        https://www.youtube.com
+// @match        https://m.youtube.com/
+// @run-at       document-start
+// @grant        GM_xmlhttpRequest
+// @namespace https://www.google.com
+// @downloadURL https://raw.githubusercontent.com/porrinha09/Better-YouTube/main/Plugins/automatic%20message.js
+// @updateURL https://raw.githubusercontent.com/porrinha09/Better-YouTube/main/Plugins/automatic%20message.js
+// ==/UserScript==
+
+alert("Hello, welcome to YouTube!");
