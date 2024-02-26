6f5790b0f3532c95d45ff395ae703395ef37546c
porrinha09 <142546604+porrinha09@users.noreply.github.com>
2024-02-26 03:51:11 -0300

    Update Mensagem automática.js

--- Plugins/Mensagem automática.js
+++ Plugins/Mensagem automática.js
@@ -2,14 +2,14 @@
 // @name         automatic message
 // @version       1.0.0
 // @description  Better YouTube
-// @author       Theawsomeyellowcatinthehat
+// @author       Kelvin zv
 // @match        https://www.youtube.com
 // @match        https://m.youtube.com/
 // @run-at       document-start
 // @grant        GM_xmlhttpRequest
 // @namespace https://www.google.com
-// @downloadURL https://github.com/porrinha09/Better-YouTube/blob/main/Plugins/Mensagem%20autom%C3%A1tica.js
-// @updateURL https://github.com/porrinha09/Better-YouTube/blob/main/Plugins/Mensagem%20autom%C3%A1tica.js
+// @downloadURL https://raw.githubusercontent.com/porrinha09/Better-YouTube/main/Plugins/Mensagem%20autom%C3%A1tica.js
+// @updateURL https://raw.githubusercontent.com/porrinha09/Better-YouTube/main/Plugins/Mensagem%20autom%C3%A1tica.js
 // ==/UserScript==
 
 alert("Olá, bem-vindo ao YouTube!");
