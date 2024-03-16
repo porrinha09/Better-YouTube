3132308535c52f8834e9668adca162b9bfa5d5de
porrinha09 <142546604+porrinha09@users.noreply.github.com>
2024-03-16 18:03:09 -0300

    Update Better YouTube.js

--- Plugins/Better YouTube.js
+++ Plugins/Better YouTube.js
@@ -1,1 +1,44 @@
-a
+// ==UserScript==
+// @name         BetterYouTube
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  Better YouTube
+// @author       Kelvin zv
+// @match        *://www.youtube.com/*
+// @grant        none
+// @license      MIT
+// ==/UserScript==
+
+const customImageURL = 'https://raw.githubusercontent.com/porrinha09/Better-YouTube/main/assets/BetterYouTube.png';
+
+const customImageSVG = `<img src="${customImageURL}" alt="Custom Image" style="width: 78px; height: 47px;">`;
+
+function isYouTubeUrl() {
+    return window.location.hostname === 'www.youtube.com' || window.location.hostname === 'youtube.com';
+}
+
+function waitForElm(selector) {
+    return new Promise(resolve => {
+        if (document.getElementById(selector)) {
+            return resolve(document.getElementById(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.getElementById(selector)) {
+                resolve(document.getElementById(selector));
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
+
+waitForElm('logo-icon').then((elm) => {
+    if (isYouTubeUrl()) {
+        document.getElementById('logo-icon').innerHTML = customImageSVG;
+    }
+});
