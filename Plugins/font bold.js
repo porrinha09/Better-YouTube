c72450346e88003b30fcdf1fd418a3a8e974e8c3
porrinha09 <142546604+porrinha09@users.noreply.github.com>
2024-04-03 15:08:29 -0300

    Update font bold.js

--- Plugins/font bold.js
+++ Plugins/font bold.js
@@ -1,1 +1,21 @@
-a
+// ==UserScript==
+// @name         font bold
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  Better Youtube
+// @author       Kelvin zv
+// @match        https://www.youtube.com/*
+// @grant        GM_addStyle
+// @downloadURL https://raw.githubusercontent.com/porrinha09/Better-YouTube/main/Plugins/font%20bold.js
+// @updateURL https://raw.githubusercontent.com/porrinha09/Better-YouTube/main/Plugins/font%20bold.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    
+    GM_addStyle(`
+        * {
+            font-weight: bold !important;
+        }
+    `);
+})();
