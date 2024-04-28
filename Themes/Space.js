cf8d74e6ac3b79d607741ad0ad420497d01498b7
porrinha09 <142546604+porrinha09@users.noreply.github.com>
2024-04-28 01:43:42 -0300

    Update Space.js

--- Themes/Space.js
+++ Themes/Space.js
@@ -1,1 +1,26 @@
-a
+// ==UserScript==
+// @name         Space
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Better YouTube
+// @icon         https://raw.githubusercontent.com/porrinha09/Better-YouTube/main/assets/BetterYouTube.png
+// @author       Kelvin zv
+// @match        https://www.youtube.com/*
+// @downloadURL https://raw.githubusercontent.com/porrinha09/Better-YouTube/main/Themes/Space.js
+// @updateURL    https://raw.githubusercontent.com/porrinha09/Better-YouTube/main/Themes/Space.js
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    GM_addStyle(`
+        body, #page-manager {
+            background-color: #121019 !important;
+            color: #fff !important;
+        }
+        ytd-masthead, #masthead-container {
+            background-color: #1B1825 !important;
+        }
+    `);
+})();
