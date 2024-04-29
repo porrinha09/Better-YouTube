33bad25aa5d6c95358c8f4f782ffa38b60846d9f
porrinha09 <142546604+porrinha09@users.noreply.github.com>
2024-04-29 14:01:35 -0300

    Update Password.js

--- Plugins/Password.js
+++ Plugins/Password.js
@@ -1,1 +1,32 @@
-a
+// ==UserScript==
+// @name         Password
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  Better Youtube
+// @author       Kelvin zv
+// @match        https://www.youtube.com/*
+// @downloadURL https://raw.githubusercontent.com/porrinha09/Better-YouTube/main/Plugins/Password.js
+// @updateURL https://raw.githubusercontent.com/porrinha09/Better-YouTube/main/Plugins/Password.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var senhaCorreta = "teste";
+
+    if (window.location.hostname === "www.youtube.com" && !localStorage.getItem("senhaDigitada")) {
+        function exibirMenuSenha() {
+            var senha = prompt("Digite a senha para acessar o YouTube:");
+
+            if (senha === senhaCorreta) {
+                localStorage.setItem("senhaDigitada", "true");
+            } else {
+                alert("Senha incorreta");
+                exibirMenuSenha();
+            }
+        }
+
+        exibirMenuSenha();
+    }
+})();
